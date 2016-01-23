05933017d4c253d583066181abc0fcf8f7a42133
Raphael Chang <raphaelchang1227@gmail.com>
2016-01-23 16:01:13 -0500

    stl export

--- src/merge.cpp
+++ src/merge.cpp
@@ -11,6 +11,11 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/search/kdtree.h>
 #include <pcl/kdtree/kdtree.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/surface/gp3.h>
+#include <pcl/io/vtk_lib_io.h>
 
 using namespace std;
 using namespace pcl::search;
@@ -59,9 +64,52 @@ bool Node::performMerge(std_srvs::Trigger::Request &req, std_srvs::Trigger::Resp
   icp.setInputSource(pcl::PointCloud<pcl::PointXYZ>::Ptr(&sideCloud));
   icp.setInputTarget(pcl::PointCloud<pcl::PointXYZ>::Ptr(&topCloud));
   icp.align(resultCloud);
-  sensor_msgs::PointCloud2 cloud;
-  pcl::toROSMsg(resultCloud, cloud);
-  mergedPublish.publish(cloud);
+  sensor_msgs::PointCloud2 cloud2;
+  pcl::toROSMsg(resultCloud, cloud2);
+  mergedPublish.publish(cloud2);
+
+
+  // STL Export
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(&resultCloud);
+  pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> n;
+  pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud<pcl::Normal>);
+  pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
+  tree->setInputCloud (cloud);
+  n.setInputCloud (cloud);
+  n.setSearchMethod (tree);
+  n.setKSearch (20);
+  n.compute (*normals);
+  //* normals should not contain the point normals + surface curvatures
+
+  // Concatenate the XYZ and normal fields*
+  pcl::PointCloud<pcl::PointNormal>::Ptr cloud_with_normals (new pcl::PointCloud<pcl::PointNormal>);
+  pcl::concatenateFields (*cloud, *normals, *cloud_with_normals);
+  //* cloud_with_normals = cloud + normals
+
+  // Create search tree*
+  pcl::search::KdTree<pcl::PointNormal>::Ptr tree2 (new pcl::search::KdTree<pcl::PointNormal>);
+  tree2->setInputCloud (cloud_with_normals);
+
+  // Initialize objects
+  pcl::GreedyProjectionTriangulation<pcl::PointNormal> gp3;
+  pcl::PolygonMesh triangles;
+
+  // Set the maximum distance between connected points (maximum edge length)
+  gp3.setSearchRadius (0.025);
+
+  // Set typical values for the parameters
+  gp3.setMu (2.5);
+  gp3.setMaximumNearestNeighbors (100);
+  gp3.setMaximumSurfaceAngle(M_PI/4); // 45 degrees
+  gp3.setMinimumAngle(M_PI/18); // 10 degrees
+  gp3.setMaximumAngle(2*M_PI/3); // 120 degrees
+  gp3.setNormalConsistency(false);
+
+  // Get result
+  gp3.setInputCloud (cloud_with_normals);
+  gp3.setSearchMethod (tree2);
+  gp3.reconstruct (triangles);
+  pcl::io::savePolygonFileSTL("out.stl", triangles);
   return true;
 }
 
