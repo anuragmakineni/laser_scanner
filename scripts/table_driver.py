25d07df0317d79994bfa1bc666e31d17b9d30cb1
Anurag Makineni <anurag.makineni@gmail.com>
2016-01-23 14:53:31 -0500

    working laser driver, table driver, and launch files

--- scripts/table_driver.py
+++ scripts/table_driver.py
@@ -11,76 +11,97 @@ import tf
 import numpy as np
 
 
+class table_driver(object):
+    def __init__(self):
+        rospy.init_node('table_driver')
+        self.angle = 0
+        self.increment = 0.05
+        self.reverse_state = 1
 
+        self.broadcaster = tf.TransformBroadcaster()
 
-def table_driver():
-	angle = 0
-	increment = 0.05
+        self.laser_side_sub = rospy.Subscriber("laser/scan1", LaserScan, self.laser_1_cb)
+        self.laser_top_sub = rospy.Subscriber("laser/scan2", LaserScan, self.laser_2_cb)
+        self.motor_sub = rospy.Subscriber("/motor_controller/state", JointState, self.motor_cb)
 
-	reverse_state = False
+        self.motor_pub = rospy.Publisher("/motor_controller/command", Float64, queue_size=10)
 
-	rospy.init_node('table_driver',anonymous=True)
-	rate = rospy.Rate(100)
-	init_time = rospy.get_rostime().to_sec()
-	
-	broadcaster = tf.TransformBroadcaster()
+        self.laser_1_called = False
+        self.laser_2_called = False
 
-	laser_side_sub = message_filters.Subscriber("laser/scan1", LaserScan)
-	laser_top_sub = message_filters.Subscriber("laser/scan2", LaserScan)
-	motor_sub = rospy.Subscriber( "/motor_controller/state", JointState, motor_cb )
-	
-	motor_pub = rospy.Publisher("/motor_controller/command", Float64, queue_size=10 )
+    def laser_1_cb(self, data):
+        """
+        save an angle 0 to 2pi, then back to 0. random small number increment (.05)
+        publish motor_pub.publish(angle)
+        """
 
-	
-	ts = message_filters.TimeSynchronizer([laser_side_sub, laser_top_sub], 10)
-	ts.registerCallback(laser_cb)
+        # rospy.loginfo("laser1!")
 
-	print("hello")
-	rospy.spin()
+        self.laser_1_called = True
 
+        if self.laser_1_called and self.laser_2_called:
+            self.increment = 0.017
+            if self.angle < 0:
+                self.reverse_state = 1
+            elif self.angle > 2 * math.pi:
+                self.reverse_state = -1
 
-def laser_cb(data):
-	"""
-	save an angle 0 to 2pi, then back to 0. random small number increment (.05)
-	publish motor_pub.publish(angle) 	
-	"""
-	if reverse_state == True:
-		increment = -0.05
-	if reverse_state == False:
-		increment = 0.05
-	if angle < 0 or angle >2*math.pi:
-		reverse_state = -reverse_state
-		
-	angle+=increment
-		
-	motor_pub.publish(angle)
-	pass
+            self.angle += self.reverse_state * self.increment
 
-def motor_cb(data):
-	"""
-	do the tf thing, get the angle from motor state (position field), 0 to 4095. convert that to radians
-	then put it into the broadcaster.sendTransfoorm.... anurag wrote
-		4095 = x 2pi
-	"""
-	position = data.current_pos
-	
-	radius = 0.5 #CHANGE
+            self.laser_1_called = False
+            self.laser_2_called = False
+
+            self.motor_pub.publish(self.angle)
+
+    def laser_2_cb(self, data):
+        """
+        save an angle 0 to 2pi, then back to 0. random small number increment (.05)
+        publish motor_pub.publish(angle)
+        """
+
+        # rospy.loginfo("laser2!")
+
+        self.laser_2_called = True
+
+        if self.laser_1_called and self.laser_2_called:
+            self.increment = 0.017
+            if self.angle < 0:
+                self.reverse_state = 1
+            elif self.angle > 2 * math.pi:
+                self.reverse_state = -1
+
+            self.angle += self.reverse_state * self.increment
+
+            self.laser_1_called = False
+            self.laser_2_called = False
+
+            self.motor_pub.publish(self.angle)
+
+    def motor_cb(self, data):
+        """
+        do the tf thing, get the angle from motor state (position field), 0 to 4095. convert that to radians
+        then put it into the broadcaster.sendTransfoorm.... anurag wrote
+            4095 = x 2pi
+        """
+        position = data.current_pos
+
+        radius = 0.5  # CHANGE
         x = np.cos(position) * radius
         y = np.sin(position) * radius
 
-	broadcaster.sendTransform((x, y, 0), tf.transformations.quaternion_from_euler(-math.pi/2, 0,  position + math.pi), rospy.Time.now(),"laser1", "world")
-	
-        broadcaster.sendTransform((0, 0, radius), tf.transformations.quaternion_from_euler(0, math.pi/2, position), rospy.Time.now(),   "laser2", "world")
+        self.broadcaster.sendTransform((x, y, 0),
+                                       tf.transformations.quaternion_from_euler(-math.pi / 2, 0, position + math.pi),
+                                       rospy.Time.now(), "laser1", "world")
 
-	print("motorcb")
-	pass
-	
+        self.broadcaster.sendTransform((0, 0, radius),
+                                       tf.transformations.quaternion_from_euler(0, math.pi / 2, position),
+                                       rospy.Time.now(), "laser2", "world")
 
-
-	
 
 if __name__ == '__main__':
+    node = table_driver()
     try:
-        table_driver()
+        rospy.spin()
+        node.stop()
     except rospy.ROSInterruptException:
-        pass
+        print("Shutting Down.")
