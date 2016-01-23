4402b07f890585b6dd856fd7f294da56a160da3f
Raphael Chang <raphaelchang1227@gmail.com>
2016-01-22 23:58:37 -0500

    merge subscribing

--- src/merge.cpp
+++ src/merge.cpp
@@ -1,4 +1,11 @@
 #include <ros/ros.h>
+#include <string>
+#include "std_msgs/String.h"
+#include "std_srvs/Trigger.h"
+#include <pcl/point_types.h>
+#include <pcl/common/projection_matrix.h>
+
+using namespace std;
 
 namespace merge {
 
@@ -8,19 +15,34 @@ class Node {
 
  private:
   ros::NodeHandle pnh_;
+  vector<pcl::PointCloud<pcl::PointXYZ> > clouds;
 };
 
 Node::Node(const ros::NodeHandle& pnh) : pnh_(pnh) {
 }
 
-
-
 }  // namespace merge
+
+void receivePointCloud(const std_msgs::String::ConstPtr& msg)
+{
+  pcl::PointCloud<pcl::PointXYZ> cloud;
+  
+}
+
+bool performMerge(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response& res)
+{
+  ROS_INFO("sdkjf");
+  return true;
+}
 
 int main(int argc, char** argv) {
   ros::init(argc, argv, "merge");
   ros::NodeHandle pnh("~");
   merge::Node node(pnh);
+  ros::init(argc, argv, "listener");
+  ros::NodeHandle n;
+  ros::Subscriber sub = n.subscribe("project", 1000, receivePointCloud);
+  ros::ServiceServer service = pnh.advertiseService("finish", performMerge);
   ros::spin();
   return 0;
 }
